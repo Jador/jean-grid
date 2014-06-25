b2e8a4b7e48cb14ac43f05d5b187646b6b3462fe
Tyler Graham <tyler.graham.prog@gmail.com>
2014-06-25 15:49:55 -0500

    Added build stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var css = require('gulp-minify-css');
 
-//TODO gulpy stuff
+var del = require('del');
 
-gulp.task('default', []);
+gulp.task('default', ['build']);
+
+gulp.task('build', [
+  'scripts',
+  'styles'
+]);
+
+gulp.task('clean', function(cb) {
+  console.log(cb);
+  del(['dist'], cb);
+})
+
+
+gulp.task('scripts', ['clean'], function() {
+  gulp.src(['!gulpfile.js','*.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', ['clean'], function() {
+  gulp.src('*.css')
+    .pipe(css())
+    .pipe(gulp.dest('dist'));
+})
