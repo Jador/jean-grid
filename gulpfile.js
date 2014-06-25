bd902714bc1bd77fa17260487b6c6b622081906e
Tyler Graham <tyler.graham.prog@gmail.com>
2014-06-25 16:04:49 -0500

    Updated build a little

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var css = require('gulp-minify-css');
-
+var rename = require('gulp-rename');
 var del = require('del');
 
 gulp.task('default', ['build']);
@@ -14,17 +14,22 @@ gulp.task('build', [
 gulp.task('clean', function(cb) {
   console.log(cb);
   del(['dist'], cb);
-})
-
+});
 
 gulp.task('scripts', ['clean'], function() {
   gulp.src(['!gulpfile.js','*.js'])
     .pipe(uglify())
+    .pipe(rename(min))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('styles', ['clean'], function() {
   gulp.src('*.css')
     .pipe(css())
+    .pipe(rename(min))
     .pipe(gulp.dest('dist'));
-})
+});
+
+function min(path) {
+  path.basename += '.min';
+}
